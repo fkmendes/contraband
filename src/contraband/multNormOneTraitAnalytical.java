4a70510342478ad695407e72653e91d64c69df18
Fabio K. Mendes <binhologia@gmail.com>
2019-02-01 00:01:36 +1300

    BM likelihood OK.

--- src/contraband/multNormOneTraitAnalytical.java
+++ src/contraband/multNormOneTraitAnalytical.java
@@ -1,54 +1,28 @@
 package contraband;
 
-import org.apache.commons.math3.linear.Array2DRowRealMatrix;
-import org.apache.commons.math3.linear.ArrayRealVector;
-import org.apache.commons.math3.linear.LUDecomposition;
 import org.apache.commons.math3.linear.RealMatrix;
 import org.apache.commons.math3.linear.RealVector;
 
 public class multNormOneTraitAnalytical {
+	
+	public double computeLk(int n, double var, RealVector mean, RealVector data, RealMatrix invVcvMat, double varToNdetTMat) {		
+		
+		/*
+		 * This whole thing is the normalizing constant the guarantees
+		 * an integral of one (a proper density function)
+		 */
+		double likelihood = 1 / ( Math.pow( (2 * Math.PI), n/2 ) *
+				Math.pow( varToNdetTMat, 0.5 ) );
+		
+		/*
+		 * Now we multiply by the data stuff
+		 */
+		likelihood *= Math.exp( 
+				invVcvMat
+				.preMultiply(data.subtract(mean)).mapMultiply(-0.5)
+				.dotProduct(data.subtract(mean))
+				);
 
-	private double var; // evolutionary rate
-	private double[] meanInput;
-	private RealVector mean;
-	
-	private double[] dataInput;
-	private RealVector data;
-	
-	private double[][] vcvMat2DInput; // tree
-	private int n; // number of species
-	private RealMatrix vcvMat;
-	private LUDecomposition vcvMatLU;
-	
-	// likelihood components
-	private RealMatrix varTimesVcvMat;
-	private RealVector dataMinusMeanVarTimesVcvMat;
-	
-	/*
-	 * Constructor
-	 */
-	public multNormOneTraitAnalytical(double var, double[] meanInput, double[] dataInput, double[][] vcvMat2DInput) {
-		this.var = var;	
-		this.meanInput = meanInput;
-		this.mean = new ArrayRealVector(meanInput);
-		
-		// initializing data
-		this.dataInput = dataInput;
-		this.data = new ArrayRealVector(dataInput);
-		
-		// initializing matrix (tree)
-		this.vcvMat2DInput = vcvMat2DInput;
-		this.n = vcvMat2DInput.length;
-		this.vcvMat = new Array2DRowRealMatrix(vcvMat2DInput);
-		this.vcvMatLU = new LUDecomposition(vcvMat);
-	}
-	
-	public void computeLk(double var, RealVector mean, RealVector data, RealMatrix vcvMat, int n) {
-		double normalizingConstant = 1 / Math.pow( (2 * Math.PI * var), n/2 ) *
-				Math.pow( vcvMatLU.getDeterminant(), 0.5 );
-			
-		varTimesVcvMat = vcvMat.scalarMultiply(var);
-		dataMinusMeanVarTimesVcvMat = varTimesVcvMat.preMultiply(data.subtract(mean).mapMultiply(-0.5));
-		double dataPart = Math.exp(  );
+		return likelihood;
 	}
 }
---
+++ src/test/OUmultNormOneTraitAnalyticalTestDriver2.java
@@ -0,0 +1,41 @@
+package test;
+
+import org.apache.commons.math3.linear.Array2DRowRealMatrix;
+import org.apache.commons.math3.linear.ArrayRealVector;
+import org.apache.commons.math3.linear.LUDecomposition;
+import org.apache.commons.math3.linear.RealMatrix;
+import org.apache.commons.math3.linear.RealVector;
+
+import contraband.multNormOneTraitAnalytical;
+
+public class OUmultNormOneTraitAnalyticalTestDriver2 {
+
+	public static void main(String[] args) {
+		
+		// var, mean, n
+		int n = 4;
+		double var = 1.4822794118;
+		double[] meanInput = new double[] { 3.079142, 3.079142, 3.079142, 3.079142 };
+		RealVector mean = new ArrayRealVector(meanInput);
+				
+		// trait values
+		double[] dataInput = new double[] { 4.1, 4.5, 5.9, 0.0 };
+		RealVector data = new ArrayRealVector(dataInput);
+		
+		// tree
+		double[][] tMatInput = new double[][] {
+			{ 3.0, 2.0, 1.0, 0.0 },
+			{ 2.0, 3.0, 1.0, 0.0 },
+			{ 1.0, 1.0, 3.0, 0.0 },
+			{ 0.0, 0.0, 0.0, 3.0 }
+			};
+		RealMatrix tMat = new Array2DRowRealMatrix(tMatInput);
+		LUDecomposition tMatLUD = new LUDecomposition(tMat);
+		RealMatrix invTMat = tMatLUD.getSolver().getInverse(); // if only variance chances and not tree, we don't have to invert
+		RealMatrix invVcvMat = invTMat.scalarMultiply(1/var); // (var*tMat)^-1 = tMat^-1 / var
+		double varToNdetTMat = Math.pow(var, n) * tMatLUD.getDeterminant(); // det(var*tMat) = var^n * det(tMat)
+				
+		multNormOneTraitAnalytical lkCalculator = new multNormOneTraitAnalytical();
+		System.out.println(lkCalculator.computeLk(n, var, mean, data, invVcvMat, varToNdetTMat));
+	}
+}
