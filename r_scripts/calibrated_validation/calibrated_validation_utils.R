48e2cd5fdd7d5caa25f16a842eeca204021b0588
Fabio K. Mendes <binhologia@gmail.com>
2019-03-20 11:36:49 +1300

    Finished with stuff for running on mahuika. Need to organize code for plotting and coverage.

---
+++ r_scripts/calibrated_validation/calibrated_validation_utils.R
@@ -0,0 +1,28 @@
+get.95 <- function(a.vector) {
+    res = hdi(a.vector, cresMass=.95)
+    return(c(res[[1]], res[[2]], mean(a.vector)))
+}
+
+write.shell.script <- function(shell.scripts.path, sim.idx, time.needed, job.prefix, jar.path, xml.file.path, xml.file.prefix) {
+    shell.file.name = paste0(shell.scripts.path, xml.file.prefix, sim.idx, ".sh")
+    if (file.exists(shell.file.name)) {
+        file.remove(shell.file.name)
+    }
+    
+    xml.file.name = paste0(xml.file.path, xml.file.prefix, sim.idx, ".xml")
+
+    write(file=shell.file.name, paste(
+        paste0("#!/bin/bash -e\n#SBATCH -J ", job.prefix, sim.idx),
+        "#SBATCH -A nesi00390",
+        paste0("#SBATCH --time=", time.needed),
+        "#SBATCH --mem-per-cpu=12288",
+        "#SBATCH --cpus-per-task=1",
+        "#SBATCH --ntasks=1",
+        "#SBATCH --hint=nomultithread",
+        "#SBATCH -D ./",
+        paste0("#SBATCH -o ", job.prefix, sim.idx, "_out.txt"),
+        paste0("#SBATCH -e ", job.prefix, sim.idx, "_err.txt"),
+        paste0("\nsrun java -jar ", jar.path, " ", xml.file.name),
+        sep="\n")
+        )
+}
--- r_scripts/calibrated_validation/contraband_calibration.R
+++ r_scripts/calibrated_validation/contraband_calibration.R
@@ -3,6 +3,14 @@ library(TreeSim)
 library(ape)
 library(stringr)
 
+source("calibrated_validation_utils.R")
+
+### SCRIPT FLAGS AND PATH VARIABLES ###
+
+simulate <- TRUE
+write.xmls <- TRUE
+write.shellscripts <- TRUE
+
 n.sim <- 100
 n.spp <- 50
 ## sigma.meanlog <- 0.0
@@ -13,8 +21,19 @@ x0.sd <- 2.0
 
 template.path <- "/home/fkur465/Documents/uoa/contraband/r_scripts/calibrated_validation/BMMVNLikelihoodOneTrait_fixedtree_template.xml"
 xmlfolder.path <- "/home/fkur465/Documents/uoa/contraband/r_scripts/calibrated_validation/BMMVNOneTrait_xmls/"
-res.path <- "/home/fkur465/Documents/uoa/contraband/r_scripts/calibrated_validation/BMMVNOneTrait_results/"
+## res.path <- "/home/fkur465/Documents/uoa/contraband/r_scripts/calibrated_validation/BMMVNOneTrait_results/"
 shell.scripts.path <- "/home/fkur465/Documents/uoa/contraband/r_scripts/calibrated_validation/BMMVNOneTrait_shellscripts/"
+rdata.path <- gsub("_template.xml", ".RData", template.path)
+
+# for server analyses
+xml.file.path <- "/nesi/project/nesi00390/fkmendes/contraband/calibrated_validation/BMMVNOneTrait_xmls/"
+xml.file.prefix <- "BMMVNLikelihoodOneTrait_fixedtree_"
+res.path <- "/nesi/project/nesi00390/fkmendes/contraband/calibrated_validation/BMMVNOneTrait_results/"
+jar.path <- "/nesi/project/nesi00390/fkmendes/contraband/contraband.jar"
+time.needed <- "00:10:00"
+job.prefix <- "BMMVNOneTrait"
+
+############# DOING STUFF #############
 
 ## in ape, branch numbering is done in postorder order: to see the order of branches, you can do tr <- reorder(tr, "po"); plor(tr); edgelabels()
 
@@ -30,15 +49,18 @@ tr <- sim.bd.taxa.age(n.spp, 1, lambda, mu, age=100, mrca=TRUE)[[1]]; write.tree
 sigmas <- rexp(n.sim, rate=sigma.rate); sigmas[1] # 0.02914
 x0s <- rnorm(n.sim, mean=x0.mean, sd=x0.sd); x0s[1] # 0.1836
 datasets <- vector("list", n.sim) # storing sims
+true.param.df <- data.frame(sigmas, x0s)
 
 ## for putting on template
 traits.4.template <- vector("list", n.sim) 
 taxon.strs.4.template <- paste(paste0("<taxon id=\"", tr$tip.label, "\" spec=\"Taxon\"/>"), collapse="\n                  ")
 
 ## actually simulating and populating strs for template
-for (i in 1:n.sim) {
-    datasets[[i]] = rTraitCont(tr, model="BM", sigma=sigmas[i], theta=x0s[i])
-    traits.4.template[[i]] = paste(paste0(names(datasets[[i]]), "=", datasets[[i]]), collapse=",")
+if (simulate) {
+    for (i in 1:n.sim) {
+        datasets[[i]] = rTraitCont(tr, model="BM", sigma=sigmas[i], theta=x0s[i])
+        traits.4.template[[i]] = paste(paste0(names(datasets[[i]]), "=", datasets[[i]]), collapse=",")
+    }
 }
 
 ## MLE
@@ -46,25 +68,33 @@ for (i in 1:n.sim) {
 ## res$theta
 ## res$sigma
 
-for (sim.idx in 1:n.sim) {
-    xml.file.name = basename(gsub("template.xml", paste0(sim.idx, ".xml"), template.path))
-    if (file.exists(xml.file.name)) {
-        file.remove(xml.file.name)
-    }
-    
-    file.name = basename(gsub(".xml", "", xml.file.name))
+if (write.xmls) {
+    for (sim.idx in 1:n.sim) {
+        xml.file.name = basename(gsub("template.xml", paste0(sim.idx, ".xml"), template.path))
+        if (file.exists(paste0(xmlfolder.path, xml.file.name))) {
+            file.remove(paste0(xmlfolder.path, xml.file.name))
+        }
+        
+        file.name = basename(gsub(".xml", "", xml.file.name))
 
-    template.lines = readLines(template.path)
-    for (line in template.lines) {
-        line = gsub("\\[BMSigmaSqPriorMeanHere\\]", format(sigma.rate, nsmall=1), line)
-        ## line = gsub("\\[BMSigmaSqPriorMeanHere\\]", format(sigma.meanlog, nsmall=1), line)
-        ## line = gsub("\\[BMSigmaSqPriorStdDevHere\\]", format(sigma.sdlog, nsmall=1), line)
-        line = gsub("\\[BMMeanPriorMeanHere\\]", format(x0.mean, nsmall=1), line)
-        line = gsub("\\[BMMeanPriorStdDevHere\\]", format(x0.sd, nsmall=1), line)
-        line = gsub("\\[TreeHere\\]", write.tree(tr), line)
-        line = gsub("\\[TaxonSetHere\\]", taxon.strs.4.template, line)
-        line = gsub("\\[TraitValuesHere\\]", traits.4.template[[sim.idx]], line)
-        line = gsub("\\[FileNameHere\\]", paste0(res.path, file.name), line)
-        write(line, file=paste0(xmlfolder.path, xml.file.name), append=TRUE)
+        template.lines = readLines(template.path)
+        for (line in template.lines) {
+            line = gsub("\\[BMSigmaSqPriorMeanHere\\]", format(1/sigma.rate, nsmall=1), line)
+            ## line = gsub("\\[BMSigmaSqPriorMeanHere\\]", format(sigma.meanlog, nsmall=1), line)
+            ## line = gsub("\\[BMSigmaSqPriorStdDevHere\\]", format(sigma.sdlog, nsmall=1), line)
+            line = gsub("\\[BMMeanPriorMeanHere\\]", format(x0.mean, nsmall=1), line)
+            line = gsub("\\[BMMeanPriorStdDevHere\\]", format(x0.sd, nsmall=1), line)
+            line = gsub("\\[TreeHere\\]", write.tree(tr), line)
+            line = gsub("\\[TaxonSetHere\\]", taxon.strs.4.template, line)
+            line = gsub("\\[TraitValuesHere\\]", traits.4.template[[sim.idx]], line)
+            line = gsub("\\[FileNameHere\\]", paste0(res.path, file.name), line)
+            write(line, file=paste0(xmlfolder.path, xml.file.name), append=TRUE)
+        }
+    }
+}
+
+if (write.shellscripts) {
+    for (sim.idx in 1:n.sim) {
+        write.shell.script(shell.scripts.path, sim.idx, time.needed, job.prefix, jar.path, xml.file.path, xml.file.prefix)
     }
 }
