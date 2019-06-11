6b09e04e6ab2f3d75f09b11d8c73a90aeee47af5
Fabio K. Mendes <binhologia@gmail.com>
2019-06-11 20:15:47 +1200

    Simple intrandomwalk not mixing well rate assignments...

---
+++ r_scripts/calibrated_validation/BMShiftTwoRatesFBD_calibrated_postMCMC_plots.R
@@ -0,0 +1,117 @@
+library(ggplot2)
+library(gtools)
+library(sjPlot)
+library(ape)
+library(gridExtra)
+source("calibrated_validation_utils.R")
+
+args = commandArgs(trailingOnly=TRUE)
+
+### SCRIPT FLAGS AND PATH VARIABLES ###
+
+## cal.validation.folder <- args[1]
+cal.validation.folder <- "./"
+## rdata.path <- args[2]
+rdata.path <- "BMMVNShiftLikelihoodTwoRatesFBDOneTrait_nonultra.RData"
+## n.sim <- as.numeric(args[3])
+n.sim <- 100
+## job.prefix <- args[4]
+job.prefix <- "BMMVNShiftTwoRatesFBD"
+## n.param <- as.numeric(args[5])
+n.param <- 3
+
+## param.names <- strsplit(args[6], ",")[[1]]
+param.names <- c("sigma1", "sigma2", "mu")
+## beast.param.names <- strsplit(args[7], ",")[[1]]
+beast.param.names <- c("rateValues1", "rateValues2", "BMMean")
+## param.labs.preparse <- strsplit(args[8], ",")[[1]]
+## prior.means.preparse <- strsplit(args[9], ",")[[1]]
+## param.labs <- prior.means <- c()
+## for (i in 1:n.param) {
+##     param.labs[i] = eval(parse(text=param.labs.preparse[i]))
+##     prior.means[i] = eval(parse(text=prior.means.preparse[i]))
+## }
+## mle.param.names <- strsplit(args[11], ",")[[1]]
+
+param.labs <- c(expression(sigma[1]^2), expression(sigma[2]^2), expression(mu))
+mle.param.names <- c("sigma1.mle", "sigma2.mle", "mu.mle")
+prior.means <- c(0.2, 0.2, 0.0)
+
+tree.type <- "nonultra"
+
+res.path <- paste0(cal.validation.folder, job.prefix, "OneTrait_", tree.type, "_results/")
+res.files <- mixedsort(paste0(res.path,list.files(res.path)))
+load(rdata.path)
+
+############# DOING STUFF #############
+
+n.cols = n.param * 3
+log.df <- data.frame(matrix(ncol=n.cols, nrow=n.sim))
+names(log.df) <- as.vector(outer(c("lower", "upper", "mean"), param.names, paste, sep="."))
+cols <- seq(length.out=n.param, by=3) # 3=lower, upper, mean
+cols.2.compare <- c(3, 6)
+for (i in 1:(n.sim-1)) {
+    this.sim.df <- read.delim(res.files[i], comment.char='#')
+
+    k = 1
+    for (j in cols) {
+        beast.param.name = as.character(beast.param.names[k])
+        log.df[i,j:(j+2)] = get.95(this.sim.df[102:1001,beast.param.name])
+        k = k+1
+    }
+
+    tmp = 0
+    lower1 = log.df[i,(cols.2.compare[1]-2)]
+    upper1 = log.df[i,(cols.2.compare[1]-1)]
+    mean1 = log.df[i,cols.2.compare[1]]
+    lower2 = log.df[i,(cols.2.compare[2]-2)]
+    upper2 = log.df[i,(cols.2.compare[2]-1)]
+    mean2 = log.df[i,cols.2.compare[2]]
+    print(paste0("val1=", mean1, " val2=", mean2))
+    if (!is.na(mean1) & !is.na(mean2) & mean1 > mean2) {
+        print(paste0("mean1=", mean1, " mean2=", mean2))
+        log.df[i,(cols.2.compare[1]-2)] = lower2
+        log.df[i,(cols.2.compare[1]-1)] = upper2
+        log.df[i,cols.2.compare[1]] = mean2
+        log.df[i,(cols.2.compare[2]-2)] = lower1
+        log.df[i,(cols.2.compare[2]-1)] = upper1
+        log.df[i,cols.2.compare[2]] = mean1
+        cat("inverting rateValues")
+    }
+}
+
+# putting true values and estimated ones together
+full.df <- cbind(true.param.df, log.df)
+
+# coverage
+table((full.df$mu >= full.df$lower.mu) & (full.df$mu <= full.df$upper.mu))
+table((full.df$sigma1 >= full.df$lower.sigma1) & (full.df$sigma1 <= full.df$upper.sigma1))
+table((full.df$sigma2 >= full.df$lower.sigma2) & (full.df$sigma2 <= full.df$upper.sigma2))
+
+## nonultrametric
+## mu
+##   FALSE  TRUE
+##       6    94
+## sigma
+##   FALSE  TRUE
+##      6    94
+
+### PLOTS ###
+
+all.plots <- vector("list", n.param)
+for (i in 1:n.param) {
+    x.lab = param.labs[i]
+    min.x = min(full.df[,param.names[i]])
+    max.x = max(full.df[,param.names[i]])
+    min.y = min(full.df[,paste0("mean.",param.names[i])], na.rm=TRUE)
+    max.y = max(full.df[,paste0("mean.",param.names[i])], na.rm=TRUE)
+    all.plots[[i]] = get.plot(param.names[i], paste0("mean.",param.names[i]),
+                              min.x, max.x, min.x, max.x, x.lab, prior.means[i],
+                              full.df)
+    names(all.plots)[i] = paste0("plot", i)
+}
+list2env(all.plots, .GlobalEnv) # sending plots in list into environment so I cna use plot_grid
+
+png(paste0(cal.validation.folder, job.prefix, "_", tree.type, "_graphs.png"), height=15, width=10, unit="cm", res=300)
+plot_grid(mget(paste0("plot", 1:2)))
+dev.off()
