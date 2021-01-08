c755c650319f183683bc8af60c41b305bd693eab
ryanzhang <rzha419@aucklanduni.ac.nz>
2021-01-08 17:08:04 +1300

    rename test driver

--- src/testdrivers/BinaryDiscreteTraitsTestDriver.java
+++ src/testdrivers/BinaryDiscreteTraitsTestDriver.java
@@ -12,7 +12,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class ThresholdModelTestDriver {
+public class BinaryDiscreteTraitsTestDriver {
 
     public static void main(String[] args) {
         // tree
